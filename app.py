52f15db6a329dff73e022bca5b87a470dd61abc7
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-08-10 19:31:28 -0600

    Remplazo textos por constantes en app.py

--- app.py
+++ app.py
@@ -1,33 +1,35 @@
 import openai
 
 # Establece las credenciales de OpenAI API
-openai.api_key = "YOUR_API_KEY"
+openai.api_key = "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"
 
-# Constantes, represe
+# Constantes, representan los miembros de la conversacion
+SEMPAI = "Sempai: "
+TSUNDERE = "Tsundere: "
 
-# Define el prompt(mensaje de usuario) inicial
+# Define el prompt inicial
 ai_description = "Eres una chica tsundere y estas hablando con tu sempai."
 
 # Define los parametros para la solicitud a la API
 model_engine = "davinci"
-temperature = 0.5
+temperature = 1.0
 max_tokens = 100
-stop_sequence = "\n"
+stop_sequence = ["\n", SEMPAI]
 
 # Inicia el bucle de conversacion
 while True:
     # Obtiene la entrada del usuario
-    user_input = input("Sempai: ")
+    user_input = input(SEMPAI)
 
     # Si el usuario escribe "bye" se termina la conversacion
     if user_input.lower() == "bye":
-        print("Chatbot: Adios!")
+        print(TSUNDERE + "Adios!")
         break
 
     # Envia el prompt(mensaje de usuario) y la entrada del usuario a la API
     response = openai.Completion.create(
         engine=model_engine,
-        prompt=ai_description + "\nSempai: " + user_input + "\nAnime Girl:",
+        prompt=ai_description + f"\n{SEMPAI}" + user_input + "\n{TSUNDERE}:",
         temperature=temperature,
         max_tokens=max_tokens,
         stop=stop_sequence
@@ -37,4 +39,4 @@ while True:
     chatbot_response = response.choices[0].text.strip()
 
     # Imprime la respuesta del chatbot
-    print("Chatbot:", chatbot_response)
+    print(TSUNDERE + chatbot_response)
